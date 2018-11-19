2a3b82a3b28af4dc40932a0cc2f3022617980ba1
pbw_office <quddnr0708@gmail.com>
2018-11-19 18:56:49 +0900

    포스트 수정시 내용 표시

--- blog-frontend/src/store/modules/editor.js
+++ blog-frontend/src/store/modules/editor.js
@@ -9,18 +9,20 @@ import * as api from 'lib/api';
 const INITIALIZE = 'editor/INITIALIZE';
 const CHANGE_INPUT = 'editor/CHANGE_INPUT';
 const WRITE_POST = 'editor/WRITE_POST';
+const GET_POST = 'editor/GET_POST';
 
 // action creators
 export const initialize = createAction(INITIALIZE);
 export const changeInput = createAction(CHANGE_INPUT);
 export const writePost = createAction(WRITE_POST, api.writePost);
+export const getPost = createAction(GET_POST, api.getPost);
 
 // initial state
 const initialState = Map({
     postId: null,
     title: '',
     markdown: '',
-    tags: ''
+    tags: '',
 });
 
 // reducer
@@ -33,8 +35,17 @@ export default handleActions({
     ...pender({
         type: WRITE_POST,
         onSuccess: (state, action) => {
-            const {_id} = action.payload.data;
+            const { _id } = action.payload.data;
             return state.set('postId', _id);
+        }
+    }),
+    ...pender({
+        type: GET_POST,
+        onSuccess: (state, action) => {
+            const { title, tags, body } = action.payload.data;
+            return state.set('title', title)
+                .set('markdown', body)
+                .set('tags', tags.join(', '));
         }
     })
 }, initialState)
