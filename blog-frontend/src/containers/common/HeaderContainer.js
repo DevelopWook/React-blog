b35f25493866a81d55429e659b6ed233cf45eea6
pbw_ckstack <quddnr9407@gmail.com>
2018-11-19 13:52:50 +0900

    test

---
+++ blog-frontend/src/containers/common/HeaderContainer.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Header from 'components/common/Header';
+import { withRouter } from 'react-router-dom';
+
+class HeaderContainer extends Component {
+    render() {
+
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default HeaderContainer;
