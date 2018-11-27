e2fb32a4cc82ab0890c57d269c8559fffa45493d
pbw_office <quddnr0708@gmail.com>
2018-11-27 15:38:50 +0900

    로그인 작업
    로그인 모달 띄우기
    로그인 성공

--- blog-frontend/src/lib/api.js
+++ blog-frontend/src/lib/api.js
@@ -15,3 +15,12 @@ export const editPost = ({ id, title, body, tags }) =>
 
 export const removePost = (id) =>
     axios.delete(`/api/posts/${id}`);
+
+export const login = (password) =>
+    axios.post('/api/auth/login', { password })
+
+export const checkLogin = () =>
+    axios.get('/api/auth/check');
+
+export const logout = () =>
+    axios.post('api/auth/logout');
